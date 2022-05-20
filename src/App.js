556be7744fb99c1abefaadf4a0b75c62596db9f9
Issac Davis <trksprkz@gmail.com>
2022-05-20 12:27:31 +0700

    added jsx and react functionality

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import { useEffect } from "react";
 import './App.css'
 import SearchIcon from './search.svg'
 
+import MovieCard from "./MovieCard";
 
 
 const API_URL = "http://www.omdbapi.com?apikey=5ace3a4"
+const movie1 = {
+        "Title": "Batman Begins",
+        "Year": "2005",
+        "imdbID": "tt0372784",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg"
+}
+
 const App = () => {
     const searchMovies = async (title) => {
         const response = await fetch (`${API_URL}&s=${title}`);
@@ -16,7 +25,24 @@ const App = () => {
         searchMovies('Batman')
     }, []);
     return (
-        <h1>App loaded</h1>
+        <div className="app">
+            <h1>Movie DB</h1>
+
+            <div className="search">
+                <input
+                placeholder="Search..."
+                value="Batman"
+                onChange={() => {}}
+                />
+                <img src={SearchIcon}
+                alt="search"
+                onClick={() => {}}
+                />
+            </div>
+            <div className="container">
+               <MovieCard movie1={movie1}/> 
+            </div>
+        </div>
     );
 }
 export default App;
